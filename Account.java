d8ded25e370328f55523b6943f3375702f621d80
TienNgen <s3924427@rmit.edu.vn>
2023-04-20 22:47:42 +0700

    add to main

---
+++ Account.java
@@ -0,0 +1,12 @@
+package OOP_GroupProject;
+
+public class Account {
+    private String username;
+    private String password;
+
+    //constructor
+    public Account(){
+        username = null;
+        password = null;
+    };
+}
--- Item.java
+++ Item.java
@@ -18,6 +18,65 @@ public class Item {
         this.genre = genre;
         this.availability = true;
     }
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRentalType() {
+        return rentalType;
+    }
+
+    public void setRentalType(String rentalType) {
+        this.rentalType = rentalType;
+    }
+
+    public int getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(int loanType) {
+        this.loanType = loanType;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getRentalFee() {
+        return rentalFee;
+    }
+
+    public void setRentalFee(double rentalFee) {
+        this.rentalFee = rentalFee;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
 
 
 
