9d75813ddd77b8f30c9506f49bcc118fc2f16b68
TienNgen <s3924427@rmit.edu.vn>
2023-04-21 20:30:38 +0700

    add Rental Class

---
+++ Rental.java
@@ -0,0 +1,34 @@
+public class Rental {
+    private Item item;
+    private int amount;
+    private string status;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public string getStatus() {
+        return status;
+    }
+
+    public void setStatus(string status) {
+        this.status = status;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    public static returnItem(){
+        status == "availible";
+        System.out.print("Your Item has been return");
+    }
+
+}
