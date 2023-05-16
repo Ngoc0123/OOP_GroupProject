fc0f9141123c2cbfeef86d27602cbcb134475c95
Ngoc <s3924436@rmit.edu.vn>
2023-05-16 16:09:39 +0700

    update

--- Customer.java
+++ Customer.java
@@ -6,4 +6,42 @@ public class Customer extends Account{
   private String address;
   private String phone;
 
+  public int getLevel() {
+    return level;
+  }
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 }
